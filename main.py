073fd779cf770be4cd56e2af0823a88eabba7f3a
Stacy-Sanders <stacy.sanders081@gmail.com>
2023-01-03 16:01:06 -0500

    added TODO

--- main.py
+++ main.py
@@ -7,41 +7,23 @@ coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 
 
-def get_coffee():
-    # TODO: 1. Ask what they want to drink, repeat until user enters "off"
-    drink_choice = input(f"What would you like? ({menu.get_items()}):  ").lower()
-    # TODO: 2. Turn off the prompt when "off" is input
-    if drink_choice == "off":
-        return "off"
-    # TODO: 3. Print report when prompted
-    elif drink_choice == "report":
-        return f"{coffee_maker.report()}\n{money_machine.report()}"
-    # TODO: 4. Check if drink is available
-    else:
-        drink_info = menu.find_drink(drink_choice)
-        if drink_info:
-            availability = coffee_maker.is_resource_sufficient(drink_info)
-            if availability:
-                # TODO: 5. Accept payment, check amount, return change if necessary
-                sale = money_machine.make_payment(drink_info.cost)
-                if sale:
-                    # TODO: 6. Present drink
-                    return coffee_maker.make_coffee(drink_info)
-                else:
-                    return sale
-
-            else:
-                return availability
-
-        else:
-            return drink_info
-
-
 is_on = True
 
+
 while is_on:
-    response = get_coffee()
-    if response == "off":
+    # TODO: 1. Ask what they want to drink, repeat until user enters "off"
+    choice = input(f"What would you like? ({menu.get_items()}):  ").lower()
+    # TODO: 2. Turn off the prompt when "off" is input
+    if choice == "off":
         is_on = False
+    # TODO: 3. Print report when prompted
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    # TODO: 4. Check if drink is available
     else:
-        response
+        drink = menu.find_drink(choice)
+        if drink:
+            # TODO: 5. if resources are sufficient and correct payment, make drink
+            if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
